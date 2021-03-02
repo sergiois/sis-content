73d67e3b4ce0870678a4e690b2a4b98f9a75ac03
Sergio Iglesias <ser.iglesias@gmail.com>
2021-03-02 19:26:50 +0100

    First commit: Version 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# sis-content
-Joomla Content plugin to display custom HTML in a position of an article
+# SIS Content: sis-content
+Joomla Content plugin to display custom HTML in a position of an article.
+[GitHub Web](https://sergiois.github.io/sis-content.html "SIS Content")
+
+## Installation and Usage
+* Install the plugin using Joomla! Extension Manager
+* Enable the plugin
+* Select category or categories
+* Select position
+* Enter custom HTML
+
+* * *
+
+# Changelogs
+
+### Version 1.0.0 [2021-03-01]
+* **[Added]** Custom HTML
+
+* * *
+
+## Copyright & License
+This plugin is licensed under GNU LESSER GENERAL PUBLIC LICENSE.
+Copyright (C) 2021 [Sergio Iglesias](https://sergioiglesias.net) - All rights reserved.
---
+++ sis_content.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * @package     Joomla.Plugin
+ * @subpackage  System.SIS_Content
+ *
+ * @copyright	Copyright © 2021 SergioIglesiasNET - All rights reserved.
+ * @license		GNU General Public License v2.0
+ * @author 		Sergio Iglesias (@sergiois)
+ */
+
+use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\CMS\Factory;
+
+defined('_JEXEC') or die;
+
+class PlgContentSIS_Content extends CMSPlugin
+{	
+	protected $app;
+	protected $position;
+	
+	public function __construct(&$subject, $config)
+	{
+		parent::__construct($subject, $config);
+
+		$app = Factory::getApplication();
+		if ($app->isClient('administrator') || $app->get('offline', '0'))
+		{
+			return;
+		}
+
+		$this->position = $this->params->get('position', 2);
+	}
+
+	function onContentAfterDisplay($context, &$article, &$params, $page = 0)
+	{
+		if (($this->position  == 2 || $this->position == 4) && $context != 'com_content.category')
+		{
+			return $this->getSISContent($article);
+		}
+	}	
+	
+	public function onContentAfterTitle($context, &$article, &$params, $page = 0)
+	{
+		if ($this->position == 3 && $context != 'com_content.category') 
+		{
+			return $this->getSISContent($article);
+		}
+	}
+		
+	function onContentBeforeDisplay($context, &$article, &$params, $page=0)
+	{
+		if (($this->position == 1 || $this->position == 4) && $context != 'com_content.category')
+		{
+			return $this->getSISContent($article);
+		}
+	}
+	
+	function getSISContent($article)
+	{
+		if (!isset($article->catid))
+		{
+			$article->catid = 0;
+		}
+		
+		$categories 		= array();
+		$cats 				= $this->params->get('catids');
+		
+		if (is_array($cats))
+		{
+			$categories 	= $cats;
+		}
+		else
+		{
+			$categories[] 	= $cats;
+		}
+		
+		// Custom content
+		$custom_content		= $this->params->get('custom');
+		
+		if (in_array($article->catid, $categories))
+		{
+			$output = '';
+
+			$output .= '<div class="siscontent">';
+			$output .= $custom_content;
+			$output .= '</div>';			
+			
+			return $output;		
+		}
+	}
+}
